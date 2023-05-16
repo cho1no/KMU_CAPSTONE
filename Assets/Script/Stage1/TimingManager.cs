4fa6dd156e60a03c508de333b801977a976e4c18
rlaRhEn <dlrudehd456@gmail.com>
2023-05-17 02:30:40 +0900

    0517
    123

--- Assets/Script/Stage1/TimingManager.cs
+++ Assets/Script/Stage1/TimingManager.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimingManager : MonoBehaviour
 {
     public List<GameObject> boxNoteList = new List<GameObject>(); // 판정범위에있는지 모든 노트를 비교 
 
-    public ComboText combotext;
-    public ComboCount combocount;
-
+    [SerializeField] GameObject ComboImage;
+    [SerializeField] UnityEngine.UI.Text comboText;
     [SerializeField] Transform Center = null;
     [SerializeField] RectTransform[] timingRect = null; //판정 범위
     Vector2[] timingBoxs = null; //판정범위의 최소값x, 최대값y
 
     EffectManager effectManager;
 
-    bool redButtonPressed;
-    bool blueButtonPressed;
-    bool yellowButtonPressed;
+    public int currentCombo = 0;
+    public Button redButton, blueButton, yellowButton;
+    bool redButtonPressed = false;
+    bool blueButtonPressed = false;
+    bool yellowButtonPressed = false;
     private void Awake()
     {
         effectManager = FindObjectOfType<EffectManager>();
@@ -32,16 +34,192 @@ public class TimingManager : MonoBehaviour
             timingBoxs[i].Set(Center.localPosition.y - timingRect[i].rect.height / 2,
                               Center.localPosition.y + timingRect[i].rect.height / 2); // 최소값 = 중심 - (이미지높이/2), 최대값 = 중심 + (이미지높이/2)
         }
+        redButton.onClick.AddListener(RedButtonPressed);
+        blueButton.onClick.AddListener(BlueButtonPressed);
+        yellowButton.onClick.AddListener(YellowButtonPressed);
+
+        comboText.gameObject.SetActive(false);
+        ComboImage.SetActive(false);
     }
-    private void Update() // 혼합별 버튼동시에 사용
+    public void FeverButton()
     {
-        if (redButtonPressed && yellowButtonPressed) //빨노
+        for (int i = 0; i < boxNoteList.Count; i++)
+        {
+            float t_notePosY = boxNoteList[i].transform.localPosition.y;
+            if (boxNoteList[0].layer == 10)
+            {
+                for (int y = 0; y < timingBoxs.Length; y++)
+                {
+                    if (timingBoxs[y].x <= t_notePosY && t_notePosY <= timingBoxs[y].y)
+                    {
+                        if (y == 0)
+                            Score.instance.GetScore(70);
+                        else if (y == 1)
+                            Score.instance.GetScore(50);
+                        else if (y == 2)
+                            Score.instance.GetScore(30);
+                        if (y < timingBoxs.Length - 1) // bad
+                        {
+                            effectManager.NoteHitEffect();
+                        }
+                        boxNoteList[i].GetComponent<NoteControl>().HideNote();
+                        boxNoteList.RemoveAt(i);
+
+                        effectManager.judgeMentEffect(y);
+                        return;
+                    }
+                }
+            }
+        }
+        effectManager.judgeMentEffect(timingBoxs.Length); //Miss
+    }
+    public void RedButton()
+    {
+        for (int i = 0; i < boxNoteList.Count; i++)
+        {
+            float t_notePosY = boxNoteList[i].transform.localPosition.y;
+
+            if (boxNoteList[0].tag == "RedNote" || boxNoteList[0].tag == "RainbowNote") //수정 필요
+            {
+                for (int y = 0; y < timingBoxs.Length; y++)
+                {
+                    if (timingBoxs[y].x <= t_notePosY && t_notePosY <= timingBoxs[y].y)
+                    {
+                        if (y == 0) //퍼펙트
+                            Score.instance.GetScore(70);
+                        else if (y == 1) //쿨
+                            Score.instance.GetScore(50);
+                        else if (y == 2) //굿
+                            Score.instance.GetScore(30);
+                        if (y < timingBoxs.Length - 1) // bad
+                        {
+                            effectManager.NoteHitEffect();
+                        }
+                        boxNoteList[i].GetComponent<NoteControl>().HideNote();
+                        boxNoteList.RemoveAt(i);
+
+                        effectManager.judgeMentEffect(y);
+
+                        IncreaseCombo();
+
+                        redButtonPressed = false;
+                        blueButtonPressed = false;
+                        yellowButtonPressed = false;
+                        return;
+                    }
+                }
+            }
+        }
+        effectManager.judgeMentEffect(timingBoxs.Length); //Miss
+        ResetCombo();
+    }
+    public void BlueButton()
+    {
+        for (int i = 0; i < boxNoteList.Count; i++)
+        {
+            float t_notePosY = boxNoteList[i].transform.localPosition.y;
+
+            if (boxNoteList[0].tag == "BlueNote" || boxNoteList[0].tag == "RainbowNote") //가장 가까운걸로 바꿔야함
+            {
+                for (int y = 0; y < timingBoxs.Length; y++)
+                {
+                    if (timingBoxs[y].x <= t_notePosY && t_notePosY <= timingBoxs[y].y)
+                    {
+                        if (y == 0)
+                            Score.instance.GetScore(70);
+                        else if (y == 1)
+                            Score.instance.GetScore(50);
+                        else if (y == 2)
+                            Score.instance.GetScore(30);
+                        if (y < timingBoxs.Length - 1) // bad
+                        {
+                            effectManager.NoteHitEffect();
+                        }
+                        boxNoteList[i].GetComponent<NoteControl>().HideNote();
+
+                        boxNoteList.RemoveAt(i);
+                        effectManager.judgeMentEffect(y);
+
+                        IncreaseCombo();
+
+                        redButtonPressed = false;
+                        blueButtonPressed = false;
+                        yellowButtonPressed = false;
+                        return;
+                    }
+                }
+            }
+        }
+        effectManager.judgeMentEffect(timingBoxs.Length); //Miss
+        ResetCombo();
+    }
+
+    public void YellowButton()
+    {
+        for (int i = 0; i < boxNoteList.Count; i++)
+        {
+            float t_notePosY = boxNoteList[i].transform.localPosition.y;
+
+            if (boxNoteList[0].tag == "YellowNote" || boxNoteList[0].tag == "RainbowNote")
+            {
+                for (int y = 0; y < timingBoxs.Length; y++)
+                {
+                    if (timingBoxs[y].x <= t_notePosY && t_notePosY <= timingBoxs[y].y)
+                    {
+                        if (y == 0)
+                            Score.instance.GetScore(70);
+                        else if (y == 1)
+                            Score.instance.GetScore(50);
+                        else if (y == 2)
+                            Score.instance.GetScore(30);
+                        if (y < timingBoxs.Length - 1) // bad
+                        {
+                            effectManager.NoteHitEffect();
+                        }
+                        boxNoteList[i].GetComponent<NoteControl>().HideNote();
+                        boxNoteList.RemoveAt(i);
+
+                        effectManager.judgeMentEffect(y);
+                        IncreaseCombo();
+
+                        redButtonPressed = false;
+                        blueButtonPressed = false;
+                        yellowButtonPressed = false;
+                        return;  
+                    }
+                }
+            }
+        }
+        effectManager.judgeMentEffect(timingBoxs.Length); //Miss
+        ResetCombo();
+    }
+
+    void RedButtonPressed()
+    {
+        redButtonPressed = true;
+        CheckButtonRY();
+        CheckButtonRB();
+    }
+    void BlueButtonPressed()
+    {
+        blueButtonPressed = true;
+        CheckButtonBY();
+        CheckButtonRB();
+    }
+    void YellowButtonPressed()
+    {
+        yellowButtonPressed = true;
+        CheckButtonRY();
+        CheckButtonBY();
+    }
+    void CheckButtonRY() //빨 노
+    {
+        if (yellowButtonPressed && redButtonPressed) //빨노
         {
             for (int i = 0; i < boxNoteList.Count; i++)
             {
                 float t_notePosY = boxNoteList[i].transform.localPosition.y;
-
-                if (boxNoteList[0].tag == "YellowRedNote")
+                if (boxNoteList[0].tag == "YellowRedNote") 
                 {
                     for (int y = 0; y < timingBoxs.Length; y++)
                     {
@@ -56,31 +234,33 @@ public class TimingManager : MonoBehaviour
                             if (y < timingBoxs.Length - 1) // bad
                             {
                                 effectManager.NoteHitEffect();
-                                combotext.combo = 0;
                             }
                             boxNoteList[i].GetComponent<NoteControl>().HideNote();
                             boxNoteList.RemoveAt(i);
 
                             effectManager.judgeMentEffect(y);
+                            IncreaseCombo();
 
-                            combotext.combo++;
-                            combotext.Ani();
-                            combocount.Ani();
+                            redButtonPressed = false;
+                            blueButtonPressed = false;
+                            yellowButtonPressed = false;
                             return;
                         }
                     }
                 }
             }
             effectManager.judgeMentEffect(timingBoxs.Length); //Miss
-            combotext.combo = 0;
-            HpManager.instance.SetHp(-1);
+            ResetCombo();
         }
+    }
+
+    void CheckButtonRB() //빨파
+    {
         if (redButtonPressed && blueButtonPressed) //빨파
         {
             for (int i = 0; i < boxNoteList.Count; i++)
             {
                 float t_notePosY = boxNoteList[i].transform.localPosition.y;
-
                 if (boxNoteList[0].tag == "RedBlueNote")
                 {
                     for (int y = 0; y < timingBoxs.Length; y++)
@@ -96,25 +276,28 @@ public class TimingManager : MonoBehaviour
                             if (y < timingBoxs.Length - 1) // bad
                             {
                                 effectManager.NoteHitEffect();
-                                combotext.combo = 0;
                             }
                             boxNoteList[i].GetComponent<NoteControl>().HideNote();
                             boxNoteList.RemoveAt(i);
 
                             effectManager.judgeMentEffect(y);
+                            IncreaseCombo();
 
-                            combotext.combo++;
-                            combotext.Ani();
-                            combocount.Ani();
+                            redButtonPressed = false;
+                            blueButtonPressed = false;
+                            yellowButtonPressed = false;
                             return;
                         }
                     }
                 }
             }
             effectManager.judgeMentEffect(timingBoxs.Length); //Miss
-            combotext.combo = 0;
-            HpManager.instance.SetHp(-1);
+            ResetCombo();
         }
+    }
+
+    void CheckButtonBY() //노파
+    {
         if (blueButtonPressed && yellowButtonPressed) //노파
         {
             for (int i = 0; i < boxNoteList.Count; i++)
@@ -136,182 +319,47 @@ public class TimingManager : MonoBehaviour
                             if (y < timingBoxs.Length - 1) // bad
                             {
                                 effectManager.NoteHitEffect();
-                                combotext.combo = 0;
                             }
                             boxNoteList[i].GetComponent<NoteControl>().HideNote();
                             boxNoteList.RemoveAt(i);
 
                             effectManager.judgeMentEffect(y);
+                            IncreaseCombo();
 
-                            combotext.combo++;
-                            combotext.Ani();
-                            combocount.Ani();
+                            redButtonPressed = false;
+                            blueButtonPressed = false;
+                            yellowButtonPressed = false;
                             return;
                         }
                     }
                 }
             }
             effectManager.judgeMentEffect(timingBoxs.Length); //Miss
-            combotext.combo = 0;
-            HpManager.instance.SetHp(-1);
+            ResetCombo();
         }
     }
-    //public void FeverButton()
-    //{
-    //    for (int i = 0; i < boxNoteList.Count; i++)
-    //    {
-    //        float t_notePosY = boxNoteList[i].transform.localPosition.y;
-    //        if (boxNoteList[0].layer == 10)
-    //        {
-    //            for (int y = 0; y < timingBoxs.Length; y++)
-    //            {
-    //                if (timingBoxs[y].x <= t_notePosY && t_notePosY <= timingBoxs[y].y)
-    //                {
-    //                    Destroy(boxNoteList[i]);
-    //                    boxNoteList.RemoveAt(i);
-    //                    Debug.Log("Hit" + y);
-    //                    return;
-    //                }
-    //            }
-
-    //        }
-    //    }
-    //    Debug.Log("Miss");
-    //}
-    public void RedButton()
+    public void IncreaseCombo(int num = 1)
     {
-        redButtonPressed = true;
-        for (int i = 0; i < boxNoteList.Count; i++)
+        currentCombo += num;
+        comboText.text = string.Format("{0:#,##0}", currentCombo);
+
+        if (currentCombo > 2)
         {
-            float t_notePosY = boxNoteList[i].transform.localPosition.y;
-
-            if (boxNoteList[0].tag == "RedNote")
-            {
-                for (int y = 0; y < timingBoxs.Length; y++)
-                {
-                    if (timingBoxs[y].x <= t_notePosY && t_notePosY <= timingBoxs[y].y)
-                    {
-                        if (y == 0) //퍼펙트
-                            Score.instance.GetScore(70);
-                        else if (y == 1) //쿨
-                            Score.instance.GetScore(50);
-                        else if (y == 2) //굿
-                            Score.instance.GetScore(30);
-                        if (y < timingBoxs.Length - 1) // bad
-                        {
-                            effectManager.NoteHitEffect();
-                            combotext.combo = 0;
-                        }
-                        boxNoteList[i].GetComponent<NoteControl>().HideNote();
-                        boxNoteList.RemoveAt(i);
-
-                        effectManager.judgeMentEffect(y);
-
-                        combotext.combo++;
-                        combotext.Ani();
-                        combocount.Ani();
-                        return;
-                    }
-                }
-            }
+            comboText.gameObject.SetActive(true);
+            ComboImage.SetActive(true);
         }
-        effectManager.judgeMentEffect(timingBoxs.Length); //Miss
-        combotext.combo = 0;
-        HpManager.instance.SetHp(-1);
+        GetCombo();
     }
-    public void RedButtonReleased()
+    public void ResetCombo()
     {
-        redButtonPressed = false;
+        currentCombo = 0;
+        comboText.text = "0";
+        comboText.gameObject.SetActive(false);
+        ComboImage.SetActive(false);
     }
-    public void BlueButton()
+    int GetCombo()
     {
-        blueButtonPressed = true;
-        for (int i = 0; i < boxNoteList.Count; i++)
-        {
-            float t_notePosY = boxNoteList[i].transform.localPosition.y;
-
-            if (boxNoteList[0].tag == "BlueNote") //가장 가까운걸로 바꿔야함
-            {
-                for (int y = 0; y < timingBoxs.Length; y++)
-                {
-                    if (timingBoxs[y].x <= t_notePosY && t_notePosY <= timingBoxs[y].y)
-                    {
-                        if (y == 0)
-                            Score.instance.GetScore(70);
-                        else if (y == 1)
-                            Score.instance.GetScore(50);
-                        else if (y == 2)
-                            Score.instance.GetScore(30);
-                        if (y < timingBoxs.Length - 1) // bad
-                        {
-                            effectManager.NoteHitEffect();
-                            combotext.combo = 0;
-                        }
-                        boxNoteList[i].GetComponent<NoteControl>().HideNote();
-
-                        boxNoteList.RemoveAt(i);
-                        effectManager.judgeMentEffect(y);
-
-                        combotext.combo++;
-                        combotext.Ani();
-                        combocount.Ani();
-                        return;
-                    }
-                }
-            }
-        }
-        effectManager.judgeMentEffect(timingBoxs.Length); //Miss
-        combotext.combo = 0;
-        HpManager.instance.SetHp(-1);
+        return currentCombo;
     }
 
-    public void BlueButtonReleased()
-    {
-        blueButtonPressed = false;
-    }
-    public void YellowButton()
-    {
-        yellowButtonPressed = true;
-        for (int i = 0; i < boxNoteList.Count; i++)
-        {
-            float t_notePosY = boxNoteList[i].transform.localPosition.y;
-
-            if (boxNoteList[0].tag == "YellowNote")
-            {
-                for (int y = 0; y < timingBoxs.Length; y++)
-                {
-                    if (timingBoxs[y].x <= t_notePosY && t_notePosY <= timingBoxs[y].y)
-                    {
-                        if (y == 0)
-                            Score.instance.GetScore(70);
-                        else if (y == 1)
-                            Score.instance.GetScore(50);
-                        else if (y == 2)
-                            Score.instance.GetScore(30);
-                        if (y < timingBoxs.Length - 1) // bad
-                        {
-                            effectManager.NoteHitEffect();
-                            combotext.combo = 0;
-                        }
-                        boxNoteList[i].GetComponent<NoteControl>().HideNote();
-
-                        boxNoteList.RemoveAt(i);
-                        effectManager.judgeMentEffect(y);
-
-                        combotext.combo++;
-                        combotext.Ani();
-                        combocount.Ani();
-                        return;  
-                    }
-                }
-            }
-        }
-        effectManager.judgeMentEffect(timingBoxs.Length); //Miss
-        combotext.combo = 0;
-        HpManager.instance.SetHp(-1);
-    }
-    public void YellowButtonReleased()
-    {
-        yellowButtonPressed = false;
-    }
 }
